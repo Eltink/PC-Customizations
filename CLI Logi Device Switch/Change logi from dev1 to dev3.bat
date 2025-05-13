d681b2b6e187fc06b0e0ae058679e9ff1d1c4850
Eltink <64210683+Eltink@users.noreply.github.com>
2025-05-13 15:18:50 +0200

    Add files via upload

---
+++ CLI Logi Device Switch/Change logi from dev1 to dev3.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo changing mouse second try
+C:\Users\glauc\OneDrive\Documents\Projetos\hidapitester.exe --vidpid 046D:C52B --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,0x02,0x0A,0x1b,0x02,0x00,0x00
+
+echo changing keybo to device 2
+C:\Users\glauc\OneDrive\Documents\Projetos\hidapitester.exe --vidpid 046D:C548 --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,0x01,0x0A,0x1b,0x02,0x00,0x00
+
+echo Done sending commands
---
+++ CLI Logi Device Switch/Change logi from dev2 to dev1.bat
@@ -0,0 +1,10 @@
+@echo off
+
+echo changing mouse - B023
+C:\Users\ge87359\Documents\hidapitester.exe --vidpid 046D:B023 --usage 0x0202 --usagePage 0xff43 --open --length 11 --send-output 0x11,0x01,0x0A,0x1b,0x00,0x00,0x00
+
+echo changing keybo (B=0x00) to device 1
+C:\Users\ge87359\Documents\hidapitester.exe --vidpid 046D:b378 --usage 0x0202 --usagePage 0xff43 --open --length 11 --send-output 0x11,0x00,0x0A,0x1b,0x00,0x00,0x00
+rem																    AA   BB   CC   DD   EE   FF   GG
+
+echo Done sending commands
